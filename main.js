7e79fa369e60a3d4bf32e3f10abf109ae7eb4e78
Emmanuel Omari Mireku <omariemmanuel91@gmail.com>
2022-11-23 19:43:27 +0000

    username validated

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var pass = document.getElementById("pass")
 var user = document.getElementById("username");
 var error_msg = document.getElementById("error-msg");
+var regex = /^([A-Z a-z])\w+/g;//rgular expression to makke sure username is valid
+var isExisting = regex.test(user);
 
 
 function validate(){
@@ -11,8 +13,12 @@ return user_validate();
 
 }
 
+function regVal(){
+
+}
+
 function user_validate(){
-  if (user.value < 5 || (/[0-9]/.test(user.value))){
+  if (user.value < 5 ||  !isExisting){
     alert("error");
     return false;
   }
