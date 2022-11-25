f0a5d80c4984160797d01e0ab6af81889ccc89bf
Emmanuel Omari Mireku <omariemmanuel91@gmail.com>
2022-11-25 13:57:32 +0000

    reg-ex for login page success

--- main.js
+++ main.js
@@ -2,19 +2,29 @@ var pass = document.getElementById("pass");
 var user = document.getElementById("username");
 var loginForm = document.getElementById("login-form");
 var usernameError = document.getElementById("uName_error");
-var inputBox = document.getElementById("username");
+// var inputBox = document.getElementById("username");
 
 user.addEventListener("input", function (e) {
   var pattern =
-    /^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$/;
+    /^[a-zA-Z](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){2,18}[a-zA-Z0-9]$/;
   var currentValue = e.target.value;
 
   var valid = pattern.test(currentValue);
   if (!valid) {
-    usernameError.style.display = "block";
-    inputBox.style.border = "1px solid red";
+    user.style.border = "1px solid red";
   } else {
-    usernameError.style.display = "none";
-    inputBox.style.border = "1px solid green";
+    user.style.border = "1px solid green";
   }
+
+  pass.addEventListener("input", function (e) {
+    var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    var currentValue = e.target.value;
+
+    var valid = pattern.test(currentValue);
+    if (!valid) {
+      pass.style.border = "1px solid red";
+    } else {
+      pass.style.border = "1px solid green";
+    }
+  });
 });
