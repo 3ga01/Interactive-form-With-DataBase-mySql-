5db90df9db79c2e1d8f4f875df0e62669d6ad02d
Emmanuel Omari Mireku <omariemmanuel91@gmail.com>
2022-11-25 13:22:13 +0000

    Username Validation with regex successfully fixed

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 var pass = document.getElementById("pass");
 var user = document.getElementById("username");
-const error_msg = document.getElementById("error-msg");
-var regex = /^([A-Z a-z])\w+/g;
-var isExisting = regex.test(user);
+var loginForm = document.getElementById("login-form");
+var usernameError = document.getElementById("uName_error");
+var inputBox = document.getElementById("username");
 
-function validate() {
-  
-}
+user.addEventListener("input", function (e) {
+  var pattern =
+    /^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$/;
+  var currentValue = e.target.value;
+
+  var valid = pattern.test(currentValue);
+  if (!valid) {
+    usernameError.style.display = "block";
+    inputBox.style.border = "1px solid red";
+  } else {
+    usernameError.style.display = "none";
+    inputBox.style.border = "1px solid green";
+  }
+});
